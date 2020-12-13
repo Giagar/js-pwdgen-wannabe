85f0a7bc89ecb337e8ba07c19eb180d9feb1d31f
Gianpaolo <pao.barga@gmail.com>
2020-12-13 18:31:35 +0100

    final commit

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Acquisizione informazioni
-var nome = prompt("Qual è il tuo nome?");
-var cognome = prompt("Qual è il tuo cognome?");
-var colore = prompt("Qual è il tuo coloro preferito?");
+var nome = prompt('Qual è il tuo nome?');
+var cognome = prompt('Qual è il tuo cognome?');
+var colore = prompt('Qual è il tuo coloro preferito?');
 var dataAttuale = new Date().getFullYear();
 
-// Generatore di password
+// // Generatore di password
 var risultato = nome + cognome + colore + dataAttuale;
 
 // Password su html
-document.getElementById("password").innerText = risultato;
+document.getElementById('password-container').innerText = risultato;
+
+// Grafica password
+document.getElementById('password-container').classList.add("password-container-style");
